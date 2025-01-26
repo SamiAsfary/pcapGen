7f7104203713b6a0f9633db95adeade6ec35a59c
SamiAsfary <s.asfary@gmail.com>
2025-01-26 22:55:50 +0100

    Added IPv4 layer

--- pcapGen.py
+++ pcapGen.py
@@ -15,6 +15,12 @@ SnapLen = b'\x00\x00\xFF\xFF'
 
 LinkType = b'\x00\x00\x00\x01'
 
+def checksumIP(headerIP):
+    checksum = 0
+    for loop in range(len(headerIP)):
+        checksum += int.from_bytes(headerIP[loop*2:loop*2+1],'big')
+    return (checksum%65536).to_bytes(2,'big')
+
 def ReturnBytes(Bytes):
     if(ENDIAN == "big"):
         return Bytes
@@ -50,6 +56,29 @@ def pcapEthernetII(srcMAC,dstMAC):
     frame += srcMAC
     frame += dstMAC
     frame += b'\x08\x00'
+    frame += pcapIPv4(mySrcIP,myDstIP)
+    return frame
+
+def pcapIPv4(srcIP, dstIP):
+    lengthHeader = 5
+    lengthData = lengthHeader*4 #suppose 0 options
+    data = b''
+    lengthData += len(data) #suppose 0 data
+    frame = b''
+    octetBuffer = b'\x40'
+    octetBuffer = octetBuffer[0] | lengthHeader.to_bytes(1)[0]
+    type(octetBuffer)
+    type(frame)
+    frame += octetBuffer.to_bytes(1,'big')
+    frame += b'\x00'
+    frame += lengthData.to_bytes(2, byteorder = 'big')
+    frame += b'\x1F\xCA' # Identification
+    frame += b'\x00\x00' # flag + fragment
+    frame += b'\x40\x11' # TTL + Protocol UDP
+    frameAddr = srcIP + dstIP
+    frame += checksumIP(frame + b'\x00\x00' + frameAddr)
+    frame += frameAddr
+    frame += data
     return frame
 
 with open(STDOUT, "wb") as f:
